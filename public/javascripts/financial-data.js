839ac45d5b563061d3f5a8ce11280bb4fd9cc395
trangnguyen <nttrang30792@gmail.com>
2020-09-15 21:29:27 +0200

    iteration 2 ok

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,5 +1,37 @@
+
 axios.get('http://api.coindesk.com/v1/bpi/historical/close.json').then(response => {
     console.log(response.data)
+    console.log('then')
+    printTheChart(response.data.bpi)
+    
 }).catch(err => {
+    console.log('catch')
     console.log('Erreur')
 })
+
+
+function printTheChart(stockData) {
+    
+    const date = Object.keys(stockData);
+    const price = Object.values(stockData);
+    console.log(date);
+    console.log(price);
+   
+   
+    const ctx = document.getElementById('my-chart').getContext('2d');
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: date,
+        datasets: [
+          {
+            label: 'Stock Chart',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: price
+          }
+        ]
+      }
+    }); // closes chart = new Chart()
+  } // closes printTheChart()
+  
